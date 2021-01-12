f5184d2eb310fc495ba20e07d1eb24d8ed885cd1
linmiaohong <625321521@qq.com>
2021-01-12 10:59:49 +0800

    加油

---
+++ javalib/src/main/java/com/example/javalib/leetcode/Num28.java
@@ -0,0 +1,87 @@
+package com.example.javalib.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ */
+public class Num28 {
+    public static void main(String[] args) {
+        Num28 num = new Num28();
+//        TreeNode treeNode1 = num.initTree(new int[]{5, 4, 8, 11, 0, 13, 4, 7, 2, 0, 0, 0, 1});
+        TreeNode treeNode1 = num.initTree(new int[]{1, 2, 2, 3, 0, 0, 3, 4, 0, 0, 4});
+//        TreeNode treeNode2 = num572.initTree(new int[]{4, 11, 0, 7, 2, 0, 0});
+//        TreeNode treeNode1 = num.initTree(new int[]{1, 1});
+//        TreeNode treeNode2 = num.initTree(new int[]{1});
+        num.print(treeNode1);
+//        System.out.println(num.isBalanced(treeNode1));
+//        System.out.println(num124.ans);
+    }
+
+    public TreeNode initTree(int[] array) {
+        return buildTree(array, 0);
+    }
+
+    /**
+     * 构造二叉树，重点在于理解2 * index + 1、2 * index + 2
+     * 当你通过把完全二叉树画出来就明白了
+     * 每n层都是n-1层的2倍数据，所以当n-1层是从index开始的话，那么到n层就是2*index开头
+     *
+     * @param nums
+     * @param index
+     * @return
+     */
+    public TreeNode buildTree(int[] nums, int index) {
+        if (index >= nums.length) {
+            return null;
+        }
+        if (nums[index] == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = buildTree(nums, 2 * index + 1);
+        root.right = buildTree(nums, 2 * index + 2);
+        return root;
+    }
+
+    private void print(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.val+"\t");
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
