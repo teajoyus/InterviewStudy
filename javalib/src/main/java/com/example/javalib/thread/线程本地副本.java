137a09ce2cfa4af38eedab5154e0c2fe560e2710
linmiaohong <625321521@qq.com>
2021-03-27 15:59:47 +0800

    加油

--- javalib/src/main/java/com/example/javalib/thread/线程本地副本.java
+++ javalib/src/main/java/com/example/javalib/thread/线程本地副本.java
@@ -8,8 +8,22 @@ package com.example.javalib.thread;
 public class 线程本地副本 {
     boolean flag = false;
     String name;
-
+    public static class AA{
+    }
+    public static class BB extends AA{
+    }
+    public static void print(AA aa){
+        System.out.println("aa AA");
+        System.out.println(aa);
+    }
+    public static void print(BB bb){
+        System.out.println("aa BB");
+        System.out.println(bb);
+    }
     public static void main(String[] args) {
+        AA aa = new BB();
+        print(aa);
+
         final 线程本地副本 demo = new 线程本地副本();
         new Thread() {
             @Override
--- study/加油富途/Android框架原理or源码准备答案.md
+++ study/加油富途/Android框架原理or源码准备答案.md
@@ -8,3 +8,9 @@
 # 热修复 MultiDex源码
 # rxjava 背压
 # mmkv
+
+
+
+## OKHTTP
+
+异步（AsyncCall）&同步（RealCall） runningSyncQueue、runningASyncQueue readyQueue
